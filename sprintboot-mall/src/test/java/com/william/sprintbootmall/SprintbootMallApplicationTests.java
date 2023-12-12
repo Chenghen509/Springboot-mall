a8f9a83aa89bea596f0b46d68e5277c565d8bf9f
Chenghen509 <wwnono147@gmail.com>
2023-12-12 17:38:56 +0800

    初始化 springboot project

---
+++ sprintboot-mall/src/test/java/com/william/sprintbootmall/SprintbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.sprintbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
